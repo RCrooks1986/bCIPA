849a3f52e8c3b9cbc74f71f83c6b90a3c592b061
Richard Crooks <email@rocrooks.co.uk>
2018-12-06 20:50:03 +0000

    Format testing functions for bcipa-interactome engine

--- public/bcipa-interactome.php
+++ public/bcipa-interactome.php
@@ -16,7 +16,11 @@ if (isset($sequences) == false)
   $sequences[1] = array("Name"=>"Peptide 2","Sequence"=>"ALEKFSMDEVEEMP");
   $sequences[2] = array("Name"=>"Peptide 3","Sequence"=>"ALEDLDWEDFKHCV");
   $sequences[3] = array("Name"=>"Peptide 4","Sequence"=>"APPDEDLCEVEASP");
+
+  $testing = true;
   }
+else
+  $testing = false;
 
 //Assign Williams scores to sequences
 include 'assign-williams.php';
@@ -27,5 +31,10 @@ include 'create-interactome.php';
 //Calculate bCIPA score
 include 'bcipa-calculator.php';
 
-print_r($interactions);
+//Produce heat map
+include 'interactions-heat.php';
+
+//Print interactions to test script
+if ($testing == true)
+  print_r($interactions);
 ?>
