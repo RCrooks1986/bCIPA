a9d2a136ea38585248d6cc9120c45f33a6f55059
Richard Crooks <email@rocrooks.co.uk>
2018-12-06 20:54:09 +0000

    Change A vs B creator to use individually named arrays

--- public/create-avsbset.php
+++ public/create-avsbset.php
@@ -12,28 +12,25 @@ if ((isset($sequences1) == false) OR (isset($sequences2) == false))
   $sequences2[1] = array("Name"=>"Protein 2","Sequence"=>"AEDLMNFHFDWED");
   $sequences2[2] = array("Name"=>"Protein 3","Sequence"=>"AEDLMNFRGASWE");
   $sequences2[3] = array("Name"=>"Protein 4","Sequence"=>"AEDLMSDWRDSWE");
+
+  $testing = true;
   }
+else
+  $testing = false;
 
 //Iteratively create interactome
 $interactions = array();
-$interactionskey1 = 0;
-$interactionsend1 = count($sequences['A']);
-$interactionsend2 = count($sequences['B']);
-while ($interactionskey1 < $interactionsend1)
+foreach ($sequences1 < $protein1)
   {
-  //Key2 always returns to the first element
-  $interactionskey2 = 0;
-  while ($interactionskey2 < $interactionsend2)
+  foreach ($sequences2 < $protein2)
     {
     //Create interaction and add it to interactome array
-    $interaction = array("Name1"=>$sequences1[$interactionskey1]['Name'],"Name2"=>$sequences2[$interactionskey2]['Name'],"Sequence1"=>$sequences1[$interactionskey1]['Sequence'],"Sequence2"=>$sequences2[$interactionskey2]['Sequence']);
+    $interaction = array("Name1"=>$protein1['Name'],"Name2"=>$protein2['Name'],"Sequence1"=>$protein1['Sequence'],"Sequence2"=>$protein2['Sequence']);
     array_push($interactions,$interaction);
-
-    $interactionskey2++;
     }
-
-  $interactionskey1++;
   }
 
-print_r($interactions);
+//Print this to test script
+if ($testing == true)
+  print_r($interactions);
 ?>
